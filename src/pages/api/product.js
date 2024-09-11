160ebba9ecd3f55ed2c0dc0f5cebc1b5bd97e26b
steve.pham <quocsonpham@gmail.com>
2024-09-11 16:03:45 +0700

    add log

--- src/pages/api/product.js
+++ src/pages/api/product.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
             const response = await axios.get('https://api.bsx.exchange/products');
             res.status(200).json(response.data);
         } catch (error) {
+            console.log(error);
             res.status(500).json({ error: 'Failed to fetch data' });
         }
     } else {
