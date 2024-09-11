4b971ffbfd9eedb2aecdd755f417ddc4716b2e1e
steve.pham <quocsonpham@gmail.com>
2024-09-11 16:31:48 +0700

    add header

--- src/pages/api/product.js
+++ src/pages/api/product.js
@@ -3,8 +3,12 @@ import axios from 'axios';
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            const response = await axios.get('https://api.bsx.exchange/products');
-            res.status(200).json(response.data);
+            const response = await axios.get('https://api.bsx.exchange/products', {
+                headers: {
+                    'Accept': 'application/json',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                },
+            }); res.status(200).json(response.data);
             console.log('RESPONSE DATA', response.data);
         } catch (error) {
             console.log(error);
