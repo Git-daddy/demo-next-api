4f760ed45b065eab9b5dfba2a47fdefb66f13b69
steve.pham <quocsonpham@gmail.com>
2024-09-11 16:28:31 +0700

    add openid api

--- src/pages/api/openid.js
+++ src/pages/api/openid.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            const response = await axios.get('https://api.bsx.exchange/products');
+            const response = await axios.get('https://kingpham.us.auth0.com/.well-known/openid-configuration');
             res.status(200).json(response.data);
-            console.log('RESPONSE DATA', response.data);
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: 'Failed to fetch data' }); g
+            res.status(500).json({ error: 'Failed to fetch data' });
         }
     } else {
         res.status(405).json({ error: 'Method not allowed' });
